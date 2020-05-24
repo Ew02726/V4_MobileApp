025dda48297be3610ce6cca26151e2e140b99195
Everrick Wright <ew02726@georgiasouthern.edu>
2020-05-24 18:02:02 -0400

    LoginPage_UI

---
+++ V4/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using V4.views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace V4
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new loginPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
