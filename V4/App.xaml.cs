faf46e948082811294ed455efef6451a42956f2f
Everrick Wright <ew02726@georgiasouthern.edu>
2020-05-27 01:48:43 -0400

    SettingView

--- V4/App.xaml.cs
+++ V4/App.xaml.cs
@@ -11,7 +11,7 @@ namespace V4
         {
             InitializeComponent();
 
-            MainPage = new DashboardView();
+            MainPage = new SettingsView();
         }
 
         protected override void OnStart()
